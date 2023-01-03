cabb18b0ed4f280e806f7b9f3123ea6424503070
yairavivi4 <yavivi441@gmail.com>
2023-01-03 23:39:49 +0200

    add subs imges annimations

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,3 +12,13 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+const changeBy = () => {
+  const options = ["one", "two", "three", "four", "five"];
+  const body = document.querySelector("#root");
+  const random = options[Math.floor(Math.random() * options.length)];
+
+  body.className = random;
+};
+
+setInterval(changeBy, 4000);
