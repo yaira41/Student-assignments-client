deea388ef7a8d009c10238be3af4624081b2753b
yairavivi4 <yavivi441@gmail.com>
2023-01-16 17:59:26 +0200

    change utils

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,4 +1,4 @@
-export const classes = ["ט", "י", "יא", "יב"];
+export const classes = ["י", "יא", "יב"];
 export const innerClasses = ["יב1", "יב2", "יב3", "יב4"];
 export const SPECIAL_SUBJECTS = [
   "ת.ז.",
