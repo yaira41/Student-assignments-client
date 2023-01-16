0232b6b02337f0c6d543d0862d423a9331e94e18
yairavivi4 <yavivi441@gmail.com>
2023-01-16 18:00:33 +0200

    add the ability to change number of classes

--- client/src/components/managerRoom/ManagerRoom.jsx
+++ client/src/components/managerRoom/ManagerRoom.jsx
@@ -1,7 +1,30 @@
 import * as xlsx from 'xlsx';
 import dataService from '../../utils/dataService';
+import './managerRoom.css';
+
+const CLASSES = ['י','יא','יב']
 
 export default function ManagerRoom() {
+
+    const onUpdateClassesNumbersSubmit = () => {
+        const data = {}
+        CLASSES.forEach(classRoom => {
+            data[classRoom] = document.querySelector(`#${classRoom}`)?.value !== '' ? 
+            document.querySelector(`#${classRoom}`)?.value : 0;
+        });
+
+        const a = JSON.stringify(data);
+        console.log(a);
+
+        dataService.updateClassesNumbers(data);
+    }
+
+    const onChangeInput = (e) => {
+        if (isNaN(e.target.value)) {
+            e.target.value = e.target.value.slice(0, -1);
+          }
+          e.target.value = e.target.value.trim();
+    }
 
     const readExcel = async(e) =>{
         const file = e.target.files[0];
@@ -19,8 +42,17 @@ export default function ManagerRoom() {
     }
 
     return(
-        <div>
+        <div className='manager-room-page'>
             <input type='file' onChange={(e) => readExcel(e)} ></input>
+            <div className='classes-numbers-container'>
+                {CLASSES.map((classRoom, index) => {
+                    return( <div key={index}>
+                    <div>{classRoom}</div> <input type='text' placeholder='מספר כיתות' 
+                    id={classRoom} onChange={onChangeInput}/>    
+                    </div>
+                )})}
+                <button type="submit" onClick={onUpdateClassesNumbersSubmit}>עדכן</button>
+            </div>
         </div>
     )    
 }
