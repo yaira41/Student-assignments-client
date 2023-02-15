50b093e136ad19382b401f83960b2b20a7a03dd3
yairavivi4 <yavivi441@gmail.com>
2023-02-16 00:40:28 +0200

    add background image

--- client/src/components/Asd.jsx
+++ client/src/components/Asd.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import { useLocation } from 'react-router-dom';
 import Missions from './missions/Missions';
+import './asd.css';
 
 const Asd = () => {
 let location = useLocation();
   
 const [user] = useState(location.state);
 return (
-  <div style={{background: "white"}}>
+  <div className='back-pic'>
     <Missions
       missions={user}
     />
