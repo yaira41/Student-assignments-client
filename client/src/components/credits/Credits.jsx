254bda71391cff0dd2cbf5df65563a6d5dca311c
yairavivi4 <yavivi441@gmail.com>
2023-03-19 00:50:22 +0200

    fix credits

--- client/src/components/credits/Credits.jsx
+++ client/src/components/credits/Credits.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Credits(){
     return(
         <div>
-            האפליקציה פותחה ביוזמה וחשיבה של שירן מעטו | עיצוב ופיתוח: דוריה אביבי 0525346663
+            האפליקציה פותחה ביוזמה וחשיבה של שירן מעטו
         </div>
     )
 }
