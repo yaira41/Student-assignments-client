4a5bf0ee017168aaacd8862534a2354cca1898e1
Yair Avivi <yavivi441@gmail.com>
2023-09-03 22:22:25 +0300

    add keys and improve code

--- client/src/components/cards/Card.jsx
+++ client/src/components/cards/Card.jsx
@@ -5,16 +5,8 @@ import {FcCheckmark} from 'react-icons/fc';
 import './Card.css';
 
 export default function Card ({title, content}) {
-    const [isFinished, setIsFinished] = useState(false);
-
-    useEffect(()=> {
-        Object.keys(content).forEach(element => {
-            if(typeof(element) === 'string' && element.includes('ציון סופי')){
-                setIsFinished(true);
-                return;
-            }
-        })
-    },[])
+    const isFinished = Object.keys(content)
+        .some((element) => typeof element === "string" && element.includes("ציון סופי"));
 
     return(
         <div className="card-container">
@@ -27,14 +19,14 @@ export default function Card ({title, content}) {
                 {Object.keys(content).map((subject, index) => {
                 if(subject.includes("ציון סופי")){
                     return (
-                        <div className="final-score"> 
+                        <div key={index} className="final-score"> 
                             <div className="final-score-subject">{subject}</div>
                             <div className="final-score-grade">{content[subject]}</div>
                         </div>
                     )
                 }
                 return (
-                <p className={index%2 === 0 ? "": "gray-background"}>
+                <p key={index} className={index%2 === 0 ? "": "gray-background"}>
                     {`${subject}`} <h4 className={content[subject] === 'חסר' ? 'missing': ''}> {content[subject]}</h4>
                 </p>)})}
             </div>
