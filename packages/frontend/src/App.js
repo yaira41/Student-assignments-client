ebeae12700b9a7201decf5a2445600f329f5a490
Yair Avivi <yavivi441@gmail.com>
2025-05-12 22:44:50 +0300

    change css

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -18,10 +18,6 @@ function App() {
             onClick={() => handleTabChange("תלמידה")}
             style={{
               marginLeft: "-8px",
-              clipPath:
-                userType === "תלמידה"
-                  ? "polygon(15% 0, 100% 0, 100% 100%, 15%  100%, 0% 50%)"
-                  : "polygon(0 0, 100% 0, 100% 100%, 0 100%, 15% 50%)",
             }}
           >
             תלמידה
@@ -31,10 +27,6 @@ function App() {
             onClick={() => handleTabChange("מורה")}
             style={{
               marginRight: "-8px",
-              clipPath:
-                userType === "מורה"
-                  ? "polygon(0 0, 85% 0, 100% 50%, 85% 100%, 0% 100%)"
-                  : "polygon(0 0, 100% 0, 85% 50%, 100% 100%, 0 100%)",
             }}
           >
             מורה
